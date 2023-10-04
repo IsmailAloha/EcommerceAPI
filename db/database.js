97c0577eafd4af2167cea073ca098c2aa797ff54
Ismail <ismails@alohatechnologydev.com>
2023-10-04 18:36:29 +0530

    Fixed database access error

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const Database = require("better-sqlite3");
 
 // Promisify database operations
-const db = new Database("./ecommerce.db", {
+const db = new Database("./db/ecommerce.db", {
   verbose: console.log,
   fileMustExist: true
 });
